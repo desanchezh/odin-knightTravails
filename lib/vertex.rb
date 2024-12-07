383c18d4ef7bcf484ac88400258f83944652fbe3
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-12-07 10:19:16 -0500

    Create possible moves hash

--- lib/vertex.rb
+++ lib/vertex.rb
@@ -1,8 +1,20 @@
 class Vertex
-  attr_accessor :root, :left, :right, :up, :down
+  attr_accessor :root, :possible_moves
 
   def initialize(root)
     @root = root
-    @moves = {1 => nil, 2 => nil, 3 => nil, 4 => nil,5 => nil, 6 => nil, 7 => nil, 8 => nil}
+    @possible_moves = {
+    1 => [root[0] + 2, root[1] + 1],
+    2 => [root[0] + 1, root[1] + 2],
+    3 => [root[0] - 1, root[1] + 2],
+    4 => [root[0] - 2, root[1] + 1],
+    5 => [root[0] - 2, root[1] - 1],
+    6 => [root[0] - 1, root[1] - 2],
+    7 => [root[0] + 1, root[1] - 2],
+    8 => [root[0] + 2, root[1] - 1]}
   end
 end
+
+vertex = Vertex.new([0, 0])
+
+p vertex.possible_moves[1]
