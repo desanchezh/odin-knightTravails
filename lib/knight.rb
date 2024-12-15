8280c5ce7129fc21d1e5994496f5ad98eda20416
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-12-15 10:20:19 -0500

    Move to main.rb

--- lib/knight.rb
+++ lib/knight.rb
@@ -41,9 +41,3 @@ class Knight
   end
 end
 
-knight = Knight.new
-
-knight.knight_moves([3,3],[4,3])
-knight.knight_moves([0,0],[6,5])
-
-
--- main.rb
+++ main.rb
@@ -1,1 +1,7 @@
 require_relative 'lib/knight'
+
+knight = Knight.new
+
+knight.knight_moves([3,3],[4,3])
+knight.knight_moves([0,0],[6,5])
+
